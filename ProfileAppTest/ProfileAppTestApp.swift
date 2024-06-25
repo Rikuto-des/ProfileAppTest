95661a33bf6dda7dff2f589fc3e95583bca1522d
RikutoYasuda <ricton513@gmail.com>
2024-06-25 22:57:49 +0900

    Initial Commit

---
+++ ProfileAppTest/ProfileAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppTestApp.swift
+//  ProfileAppTest
+//
+//  Created by user on 2024/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
